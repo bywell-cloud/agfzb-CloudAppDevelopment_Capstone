b87711cdde79ab12d40615e0c6e38e003bfc5317
Bywell Singini <sbwmsg@gmail.com>
2022-05-09 15:31:21 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,10 +26,10 @@ urlpatterns = [
     path("logout", views.logout_request, name="logout"),
     path("register", views.registration_request, name="register"),
     path('api/dealership', views.dealerships, name='dealerships'),
-    path('api/dealership?state=<str:state>', views.get_dealer_state, name='get_dealer_state),
+    path('api/dealership?state=<str:state>', views.get_dealer_state, name='get_dealer_state'),
     path('api/dealership/<int:dealer_id>', views.get_dealer_details, name='get_dealer_details'),
-    
-    path('api/addreview/<int:dealer_id>/', views.add_review_s, name='add_review_s'),
+    path('api/review/<int:dealer_id>', views.get_dealer_reviewdetails, name='get_dealer_reviewdetails'),
+    path('api/addreview/<int:dealer_id>/', views.add_review, name='add_review'),
 
 
     # path for dealer reviews view

e6e06411d323e5a47a2682e1b62857edb105399e
Bywell Singini <sbwmsg@gmail.com>
2022-05-09 16:41:10 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -16,15 +16,19 @@ class CarModel(models.Model):
     name = models.CharField(null=False, max_length=30)
     carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealer_id = models.IntegerField()
+    
     SEDAN = 'sedan'
     SUV = 'suv'
     WAGON = 'wagon'
     OTHER = 'other'
     CAR_TYPE_CHOICES = [
+        (BENZ = 'Benz'),
+        (BMW , 'Bmw'),
         (SEDAN, 'Sedan'),
         (SUV, 'SuV'),
-        (WAGON, 'Wagon'),
+        (WAGON, 'Wagon'),      
         (OTHER, 'Other')
+        
     ]
     car_type = models.CharField(
         max_length=10,
@@ -37,48 +41,61 @@ class CarModel(models.Model):
         return "Name: " + self.name + "," + \
                "Type: " + self.car_type + "," + \
                "Year: " + self.year.strftime("%Y")
-
-
+    
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
-    def __init__(self, dealership):
-        for key,value in dealership.items():
-            setattr(self, key, value)
+
+    def __init__(self, id, full_name, short_name, city, address, state, st, zip, lat, long):
+        self.id = id
+        self.full_name = full_name
+        self.short_name = short_name
+        self.city = city
+        self.address = address
+        self.state = state
+        self.st = st
+        self.zip = zip
+        self.lat = lat
+        self.long = long
 
     def __str__(self):
-        return "Dealer name: " + self.full_name
+        return 'Dealer Name: ' + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
-    def __init__(self, review, sentiment):
-        for key,value in review.items():
-            setattr(self, key, value)
-        self.sentiment = sentiment
+
+    def __init__(self, id, car_make, car_model, car_year, dealership, name, purchase, purchase_date, review, sentiment):
+         self.id = id
+         self.car_make = car_make
+         self.car_model = car_model
+         self.car_year = car_year
+         self.dealership = dealership
+         self.name = name
+         self.purchase = purchase
+         self.purchase_date = purchase_date
+         self.review = review
+         self.sentiment = sentiment
 
     def __str__(self):
-        return "Dealer name: " + self.full_name
-
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+        return 'Review name ' + self.name    
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+#class CarDealerZZ:
+   # def __init__(self, dealership):
+ ##       for key,value in dealership.items():
+    #        setattr(self, key, value)
 
+    #def __str__(self):
+     #   return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+#class DealerReviewZZ:
+ #   def __init__(self, review, sentiment):
+  #      for key,value in review.items():
+   #         setattr(self, key, value)
+    #    self.sentiment = sentiment
+
+    #def __str__(self):
+     #   return "Dealer name: " + self.full_name
+
+
